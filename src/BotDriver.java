231b49187cf42db9aec448cffbd0b99fe558b07a
Emil Murman <emilmurman@hotmail.com>
2015-11-08 04:25:59 +0100

    Better UI

--- src/BotDriver.java
+++ src/BotDriver.java
@@ -1,9 +1,9 @@
-
 public class BotDriver {
 	public static String CHANNEL_NAME = "";
 	
 	public static void main(String[] args) {
 		String oAuth = "";
+		
 		if (args.length == 1) {
 			BotDriver.CHANNEL_NAME = "#hekimae";
 			oAuth = args[0];
@@ -11,22 +11,23 @@ public class BotDriver {
 			BotDriver.CHANNEL_NAME = args[0];
 			oAuth = args[1];
 		}
-		System.out.println(System.getProperty("java.runtime.version"));
+		
 		String host = "irc.twitch.tv";
 		int port = 6667;
 		
-		
+		System.out.println("Starting up the bot...");
 		HekiBot bot = new HekiBot();
-		bot.setVerbose(true);
+		//bot.setVerbose(true);
+		
 		try {
 			bot.connect(host, port, oAuth);
 			bot.joinChannel(CHANNEL_NAME);
 			bot.sendMessage(CHANNEL_NAME, "HeyGuys ");
+			System.out.println("hekibot is up and running!");
 		} 
 		catch (Exception e) {
 			System.out.println("Uhm, couldn't connect to twitch chat servers. For some reason.");
 			System.out.println(e.getMessage());
 		}
-		
 	}
 }
--- src/HekiBot.java
+++ src/HekiBot.java
@@ -1,4 +1,7 @@
-import org.jibble.pircbot.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import org.jibble.pircbot.PircBot;
 
 public class HekiBot extends PircBot {
 	
@@ -11,6 +14,7 @@ public class HekiBot extends PircBot {
 	///// Mods and people
 	private String heki = "hekimae";
 	private String lime = "limeman2";
+	private String hekibotName = "hekibot";
 	private static String[] MOD_LIST = {
 			"hekimae",
 			"7updiet",
@@ -44,6 +48,8 @@ public class HekiBot extends PircBot {
 	////// Misc vars 
 	private boolean isLastGame = false;
 	private boolean isModNight = false;
+	private SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+	private Calendar rightNow = Calendar.getInstance();
 	
 	public HekiBot() {
 		this.setName("hekibot");
@@ -57,53 +63,60 @@ public class HekiBot extends PircBot {
 		try {
 			this.reconnect();
 			this.joinChannel("#hekimae");
-			this.sendMessage("#hekimae", "I just disconnected, but now I'm back.");
+			this.sendMessageAndPrint("#hekimae", "I just disconnected, but now I'm back.");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
 
+	@Override
+	protected void onAction(String sender, String login, String hostname, String target, String action) {
+		// TODO Auto-generated method stub
+		super.onAction(sender, login, hostname, target, action);
+	}
+
 	@Override 
 	protected void onMessage(String channel, String sender, String login,
 			String hostname, String message) {
+		if (!sender.equals(hekibotName)) showChatMessage(sender, message);
 		
 		if (message.startsWith("!")) {				
 			if ((sender.equals(heki) || sender.equals(lime))) {
 				if (message.equalsIgnoreCase(clearQCmd))
-					sendMessage(channel, q.clearQueue());
+					sendMessageAndPrint(channel, q.clearQueue());
 				
 				if (message.matches("!nextq \\d")) {
-					sendMessage(channel, q.nextQueue(Integer.parseInt(Character.toString(message.charAt(7)))));
+					sendMessageAndPrint(channel, q.nextQueue(Integer.parseInt(Character.toString(message.charAt(7)))));
 				}
 				
 				if (message.equalsIgnoreCase(gNightCmd)) {
-					sendMessage(channel, "G'NIGHT ResidentSleeper");
+					sendMessageAndPrint(channel, "G'NIGHT ResidentSleeper");
 					try {
 						wait(500);
 					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 					this.disconnect();
 					this.dispose();
+					System.exit(0);
 				}
 				
 				if (message.equalsIgnoreCase(lastGameCmd)) {
 					if (!isLastGame) {
-						sendMessage(channel, "This will be the last game, folks! Sorry if you're still in queue :(");
+						sendMessageAndPrint(channel, "This will be the last game, folks! Sorry if you're still in queue :(");
 						isLastGame = true;
 					} else {
-						sendMessage(channel, "Nevermind! We'll play some more.");
+						sendMessageAndPrint(channel, "Nevermind! We'll play some more.");
 						isLastGame = false;
 					}
 				}
 				
 				if (message.equalsIgnoreCase(modNightCmd)) {
 					if (!isModNight) {
-						sendMessage(channel, "IT'S MOD NIGHT, LADIES AND GENTLEMEN!");
+						sendMessageAndPrint(channel, "IT'S MOD NIGHT, LADIES AND GENTLEMEN!");
 						isModNight = true;
 					} else {
-						sendMessage(channel, "MOD NIGHT IS NOW OVER, FOLKS!");
+						sendMessageAndPrint(channel, "MOD NIGHT IS NOW OVER, FOLKS!");
 						isModNight = false;
 					}
 					
@@ -114,78 +127,86 @@ public class HekiBot extends PircBot {
 				if (isModNight) {
 					if (isOp(sender)) {
 						if (!isLastGame) {
-							sendMessage(channel, q.addEntry(sender, 0));
+							sendMessageAndPrint(channel, q.addEntry(sender, 0));
 						} else {
-							sendMessage(channel, "Sorry! This is the last game, " + sender);
+							sendMessageAndPrint(channel, "Sorry! This is the last game, " + sender);
 						}
 					} else {
-						sendMessage(channel, "Sorry, " + sender + ", tonight is MOD NIGHT!");
+						sendMessageAndPrint(channel, "Sorry, " + sender + ", tonight is MOD NIGHT!");
 					}
 				} else {
 					if (!isLastGame) {
-						sendMessage(channel, q.addEntry(sender, 0));
+						sendMessageAndPrint(channel, q.addEntry(sender, 0));
 					} else {
-						sendMessage(channel, "Sorry! This is the last game, " + sender);
+						sendMessageAndPrint(channel, "Sorry! This is the last game, " + sender);
 					}
 				}
 			}
-			
 			
 			if (message.matches("!q\\+\\d")) {
 				if (isModNight) {
 					if (isOp(sender)) {
 						if (!isLastGame) {
-							sendMessage(channel, q.addEntry(sender, Integer.parseInt(Character.toString(message.charAt(3)))));
+							sendMessageAndPrint(channel, q.addEntry(sender, Integer.parseInt(Character.toString(message.charAt(3)))));
 						} else {
-							sendMessage(channel, "Sorry! This is the last game, " + sender);
+							sendMessageAndPrint(channel, "Sorry! This is the last game, " + sender);
 						}
 					} else {
-						sendMessage(channel, "Sorry, " + sender + ", tonight is MOD NIGHT!");
+						sendMessageAndPrint(channel, "Sorry, " + sender + ", tonight is MOD NIGHT!");
 					}
 				} else {
 					if (!isLastGame) {
-						sendMessage(channel, q.addEntry(sender, Integer.parseInt(Character.toString(message.charAt(3)))));
+						sendMessageAndPrint(channel, q.addEntry(sender, Integer.parseInt(Character.toString(message.charAt(3)))));
 					} else {
-						sendMessage(channel, "Sorry! This is the last game, " + sender);
+						sendMessageAndPrint(channel, "Sorry! This is the last game, " + sender);
 					}
 				}		
 			}
 			
 			if (message.equalsIgnoreCase(qListCmd) && isOp(sender)) {
-				sendMessage(channel, q.queueList());
+				sendMessageAndPrint(channel, q.queueList());
 			}
 			
 			
 			if (message.equalsIgnoreCase(qPosCmd)) {
 				int pos = q.queuePosition(sender);
 				if (pos == 0) {
-					sendMessage(channel, "You are not currently in the queue, " + sender);
+					sendMessageAndPrint(channel, "You are not currently in the queue, " + sender);
 				} else {
-					sendMessage(channel, "You are currently number " + Integer.toString(pos) + " in the queue, " + sender + ".");
+					sendMessageAndPrint(channel, "You are currently number " + Integer.toString(pos) + " in the queue, " + sender + ".");
 				}	
 			}
 			
 			if (message.equalsIgnoreCase(leaveQCmd1) || message.equalsIgnoreCase(leaveQCmd2)) {
 				if (isModNight) {
 					if(isOp(sender)) {
-						sendMessage(channel, q.leaveQueue(sender));
+						sendMessageAndPrint(channel, q.leaveQueue(sender));
 					} else {
-						sendMessage(channel, "Sorry, " + sender + ", tonight is MOD NIGHT!");
+						sendMessageAndPrint(channel, "Sorry, " + sender + ", tonight is MOD NIGHT!");
 					}
 				} else {
-					sendMessage(channel, q.leaveQueue(sender));
+					sendMessageAndPrint(channel, q.leaveQueue(sender));
 				}	
 			}
 			 
 			if (message.equalsIgnoreCase(oldCmd)) {
-				sendMessage(channel, "We have changed the command to !q, " + sender + ".");
+				sendMessageAndPrint(channel, "We have changed the command to !q, " + sender + ".");
 			}
 			
 			if (message.equalsIgnoreCase(commandsCmd)) {
-				sendMessage(channel, "Available commands: !q, !q+1, !q+2, !qlist (mods only), !qpos, !leaveq");
+				sendMessageAndPrint(channel, "Available commands: !q, !q+1, !q+2, !qlist (mods only), !qpos, !leaveq");
 			}
 		}
 	} 
+	
+	private void showChatMessage(String sender, String message) {
+		System.out.println(timeFormat.format(rightNow.getTime()) + " | " + sender + ": " + message);
+	}
+	
+	private void sendMessageAndPrint(String channel, String message) {
+		showChatMessage("hekibot", message);
+		sendMessage(channel, message);
+	}
 	
 	private boolean isOp(String sender) {
 		for (String name : MOD_LIST) {
