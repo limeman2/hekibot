958dddbbf3519f559e12ab988e8e013a2efa9eaa
limeman2 <emilmurman@hotmail.com>
2017-06-15 23:15:29 +0200

    Start properly tracking users in channel

---
+++ src/main/java/diet/nutella/hekibot/loyaltytracker/CurrentUsersTracker.java
@@ -0,0 +1,53 @@
+package main.java.diet.nutella.hekibot.loyaltytracker;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import main.java.diet.nutella.hekibot.model.SimpleTwitchUser;
+import main.java.diet.nutella.hekibot.model.UserDAO;
+
+public class CurrentUsersTracker {
+	private UserDAO dao;
+	private List<SimpleTwitchUser> users;
+
+	public CurrentUsersTracker(UserDAO dao) {
+		this.dao = dao;
+		this.users = new ArrayList<SimpleTwitchUser>();
+		SimpleTwitchUser[] array;
+		try {
+			array = dao.getUsersInChannel();
+			if (array != null) {
+				this.users.addAll(Arrays.asList(dao.getUsersInChannel()));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public SimpleTwitchUser[] getUsers() {
+		SimpleTwitchUser[] array = new SimpleTwitchUser[users.size()];
+		for(int i = 0; i < array.length; i++) {
+			array[i] = users.get(i);
+		}
+		return array;
+	}
+
+	public void update() {
+		SimpleTwitchUser[] array = null;
+		try {
+			array = dao.getUsersInChannel();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		users.clear();
+		if (array != null) {
+			this.users.addAll(Arrays.asList(array));
+		}
+	}
+	
+	
+	
+	
+}
