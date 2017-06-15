dd4e00b426b39ac48686522825b6ce26ef33dce3
limeman2 <emilmurman@hotmail.com>
2017-06-15 23:13:00 +0200

    Add proper tracking of users in channel

--- src/main/java/diet/nutella/hekibot/loyaltytracker/LoyaltyUpdater.java
+++ src/main/java/diet/nutella/hekibot/loyaltytracker/LoyaltyUpdater.java
@@ -2,18 +2,23 @@ package main.java.diet.nutella.hekibot.loyaltytracker;
 
 import java.util.TimerTask;
 
+import main.java.diet.nutella.hekibot.model.SimpleTwitchUser;
 import main.java.diet.nutella.hekibot.model.UserDAO;
 
 public class LoyaltyUpdater extends TimerTask {
-	
 	private UserDAO dao;
+	private CurrentUsersTracker currentUsers;
 	
-	public LoyaltyUpdater(UserDAO dao) {
+	public LoyaltyUpdater(UserDAO dao, CurrentUsersTracker currentUsers) {
 		this.dao = dao;
+		this.currentUsers = currentUsers;
 	}
 
 	@Override
-	public void run() {		
-		dao.updateLoyalty(dao.getUsersInChannel());
+	public void run() {	
+		currentUsers.update();
+		SimpleTwitchUser[] users = currentUsers.getUsers();
+		
+		dao.updateLoyalty(users);
 	}
 }
--- src/main/java/diet/nutella/hekibot/model/UserDAO.java
+++ src/main/java/diet/nutella/hekibot/model/UserDAO.java
@@ -11,6 +11,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -21,12 +25,33 @@ import main.java.diet.nutella.hekibot.controller.BotDriver;
 public class UserDAO {
 	
 	private static final String UPDATE_STATEMENT = 
-			"INSERT INTO users (name, coins, time) VALUES (?, 1, 1) ON DUPLICATE KEY UPDATE time = time + 1;";
+			"INSERT INTO users "
+			+ "(id, name, coins, time) "
+			+ "VALUES (?, ?, 1, 1) "
+			+ "ON DUPLICATE KEY UPDATE "
+			+ "coins = coins + 1,"
+			+ "time = time + 1,"
+			+ "name = ?;";
+	
 	private static final String MODIFY_COINS_STATEMENT = 
-			"INSERT INTO users (name, coins, time) VALUES (?, ?, 1) ON DUPLICATE KEY UPDATE coins = coins + ?";
+			"UPDATE users SET coins = coins + ? WHERE name = ?";
+	
+	private static final String GET_USER_BY_NAME_STATEMENT = 
+			"SELECT * FROM users WHERE name=?";
+	
+	private static final String GET_USER_BY_ID_STATEMENT =
+			"SELECT * FROM users WHERE id=?";
+	
+	private static final String GET_TOP_USERS_STATEMENT =
+			"SELECT * FROM users ORDER BY coins DESC LIMIT ?";
+	
+	private static final String GET_TOP_USERS_BY_TIME_STATEMENT =
+			"SELECT * FROM users ORDER BY time DESC LIMIT ?";
+	
 	private static final String USER_REQUEST_URL = String.format(
 			"https://tmi.twitch.tv/group/user/%s/chatters", 
 			BotDriver.CHANNEL_NAME.substring(1));
+
 	
 	private Connection conn;
 	
@@ -47,28 +72,30 @@ public class UserDAO {
 		
 		try {
 			stmt = conn.prepareStatement(MODIFY_COINS_STATEMENT);
-			stmt.setString(1, name);
-			stmt.setInt(2, amount);
-			stmt.setInt(3, amount);
+			stmt.setInt(1, amount);
+			stmt.setString(2, name);
+
 			stmt.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public void updateLoyalty(String name) {
-		String converter[] = new String[1];
-		converter[0] = name;
+	public void updateLoyalty(SimpleTwitchUser user) {
+		SimpleTwitchUser[] converter = new SimpleTwitchUser[1];
+		converter[0] = user;
 		updateLoyalty(converter);
 	}
 	
-	public void updateLoyalty(String[] names) {
+	public void updateLoyalty(SimpleTwitchUser[] users) {
 		PreparedStatement stmt = null;
 		
 		try {
 			stmt = conn.prepareStatement(UPDATE_STATEMENT);
-			for(String name : names) {
-				stmt.setString(1, name);
+			for(SimpleTwitchUser user : users) {
+				stmt.setInt(1, user.getId());
+				stmt.setString(2, user.getUserName());
+				stmt.setString(3, user.getUserName());
 				stmt.executeUpdate();
 			}
 		} catch (Exception e) {
@@ -76,19 +103,20 @@ public class UserDAO {
 		}
 	}
 	
-	public User getUserFromDatabase(String name) {
+	public UserInDB getUserFromDatabase(String name) {
 		PreparedStatement stmt = null;
 		
 		try {
-			stmt = conn.prepareStatement("SELECT * FROM users WHERE name=?");
+			stmt = conn.prepareStatement(GET_USER_BY_NAME_STATEMENT);
 			stmt.setString(1, name);
 			ResultSet rs = stmt.executeQuery();
 			rs.next();
 			
-			return new User(
-					rs.getString(1),
-					rs.getInt(2),
-					rs.getInt(3)
+			return new UserInDB(
+					rs.getInt(1),
+					rs.getString(2),
+					rs.getInt(3),
+					rs.getInt(4)
 					);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -97,64 +125,172 @@ public class UserDAO {
 		return null;
 	}
 	
-	public String[] getUsersInChannel() {
-		List<String> users = new ArrayList<String>();
+	public UserInDB[] getTopUsers(int amount) {
+		PreparedStatement stmt = null;
+		UserInDB[] users = new UserInDB[amount];
+		
 		try {
+			stmt = conn.prepareStatement(GET_TOP_USERS_STATEMENT);
+			stmt.setInt(1, amount);
+			ResultSet rs = stmt.executeQuery();
 			
-			JSONParser jp = new JSONParser();
+			int i = 0;
+			while(rs.next()) {
+				users[i] = new UserInDB(
+						rs.getInt(1),
+						rs.getString(2),
+						rs.getInt(3),
+						rs.getInt(4)
+						);
+				i++;
+			}
 			
-			URL url = new URL(USER_REQUEST_URL);
-			HttpURLConnection request = (HttpURLConnection) url.openConnection();
-			request.setRequestMethod("GET");
-			request.setRequestProperty("Accept", "application/vnd.twitchtv.v5+json");
-			request.setRequestProperty("Client-ID", BotDriver.props.getProperty("twitch-api-client-id"));
-			request.connect();
-			
-			///////// Parse JSON data into different user types
-			
-			JSONObject obj = (JSONObject) jp.parse(new InputStreamReader((InputStream) request.getContent()));
-			JSONArray staff = (JSONArray) ((JSONObject)obj.get("chatters")).get("staff");
-			JSONArray viewers = (JSONArray) ((JSONObject)obj.get("chatters")).get("viewers");
-			JSONArray globalMods = (JSONArray) ((JSONObject)obj.get("chatters")).get("global_mods");
-			JSONArray mods = (JSONArray) ((JSONObject)obj.get("chatters")).get("moderators");
-			JSONArray admins = (JSONArray) ((JSONObject)obj.get("chatters")).get("admins");
-			
-			///////// Add every user type to one big list
-			
-			users.addAll(0, staff);
-			users.addAll(0, viewers);
-			users.addAll(0, globalMods);
-			users.addAll(0, mods);
-			users.addAll(0, admins);			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return users.toArray(new String[0]);
+
+		return users;
 	}
 	
-	public String[] getMods() {
-		List<String> users = new ArrayList<String>();
-		try {
-			
-			JSONParser jp = new JSONParser();
-			
-			URL url = new URL(USER_REQUEST_URL);
-			HttpURLConnection request = (HttpURLConnection) url.openConnection();
-			request.setRequestMethod("GET");
-			request.setRequestProperty("Accept", "application/vnd.twitchtv.v5+json");
-			request.setRequestProperty("Client-ID", BotDriver.props.getProperty("twitch-api-client-id"));
-			request.connect();
-			
-			///////// Parse JSON data into different user types
-			
-			JSONObject obj = (JSONObject) jp.parse(new InputStreamReader((InputStream) request.getContent()));
-			JSONArray mods = (JSONArray) ((JSONObject)obj.get("chatters")).get("moderators");
+	public UserInDB[] getTopUsersByTime(int amount) {
+		PreparedStatement stmt = null;
+		UserInDB[] users = new UserInDB[amount];
 		
-			///////// Add mods to list
-			users.addAll(0, mods);			
+		try {
+			stmt = conn.prepareStatement(GET_TOP_USERS_BY_TIME_STATEMENT);
+			stmt.setInt(1, amount);
+			ResultSet rs = stmt.executeQuery();
+			
+			int i = 0;
+			while(rs.next()) {
+				users[i] = new UserInDB(
+						rs.getInt(1),
+						rs.getString(2),
+						rs.getInt(3),
+						rs.getInt(4)
+						);
+				i++;
+			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return users.toArray(new String[0]);
+
+		return users;
 	}
+	
+	public UserInDB getUserFromDatabase(int id) {
+		PreparedStatement stmt = null;
+		
+		try {
+			stmt = conn.prepareStatement(GET_USER_BY_ID_STATEMENT);
+			stmt.setInt(1, id);
+			ResultSet rs = stmt.executeQuery();
+			rs.next();
+			
+			return new UserInDB(
+					rs.getInt(1),
+					rs.getString(2),
+					rs.getInt(3),
+					rs.getInt(4)
+					);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+	
+	public SimpleTwitchUser[] getUsersInChannel() throws Exception {
+		JSONParser jp = new JSONParser();
+			
+		/////// Get usernames in channel
+			
+		URL url = new URL(USER_REQUEST_URL);
+		HttpURLConnection request = (HttpURLConnection) url.openConnection();
+		request.setRequestMethod("GET");
+		request.setRequestProperty("Accept", "application/vnd.twitchtv.v5+json");
+		request.setRequestProperty("Client-ID", BotDriver.props.getProperty("twitch-api-client-id"));
+		request.connect();
+		
+		///////// Parse JSON data into different user types
+		
+		JSONObject obj = (JSONObject) jp.parse(new InputStreamReader((InputStream) request.getContent()));
+		JSONArray staff = (JSONArray) ((JSONObject)obj.get("chatters")).get("staff");
+		JSONArray viewers = (JSONArray) ((JSONObject)obj.get("chatters")).get("viewers");
+		JSONArray globalMods = (JSONArray) ((JSONObject)obj.get("chatters")).get("global_mods");
+		JSONArray mods = (JSONArray) ((JSONObject)obj.get("chatters")).get("moderators");
+		JSONArray admins = (JSONArray) ((JSONObject)obj.get("chatters")).get("admins");
+		
+		///////// Add every user type to one big list of usernames
+		
+		List<String> userNamesInChannel = new ArrayList<String>();
+		
+		userNamesInChannel.addAll(0, staff);
+		userNamesInChannel.addAll(0, viewers);
+		userNamesInChannel.addAll(0, globalMods);
+		userNamesInChannel.addAll(0, mods);
+		userNamesInChannel.addAll(0, admins);	
+			
+		//////// API call to look up IDs of every user in chat
+		if (userNamesInChannel.size() == 0) return null;
+		return lookupTwitchId(userNamesInChannel).toArray(new SimpleTwitchUser[0]);
+	}
+	
+	private List<SimpleTwitchUser> lookupTwitchId(List<String> userNames) {
+		if(userNames.size() == 0) return null;
+		
+		/// Initialize array to return
+		List<SimpleTwitchUser> result = new ArrayList<SimpleTwitchUser>();
+		
+		/// Calculate how many API calls to make
+		/// Put them in batches of 100
+		int numberOfApiCalls = 1 + (userNames.size() / 100);
+		
+		/// Set up executor service and list of pending results
+		ExecutorService executor = Executors.newFixedThreadPool(50);
+		List<Future<SimpleTwitchUser[]>> pendingResults = 
+				new ArrayList<Future<SimpleTwitchUser[]>>();
+		
+		/// Tell the executor to make API calls
+		while (numberOfApiCalls > 0) {
+			/// Prepare one batch of up to 100 users to request
+			List<String> batch = new ArrayList<String>();
+			for(int i = 0; i < 100 && userNames.size() > 0; i++) {
+				batch.add(userNames.remove(0));
+			}
+			
+			/// Instantiate requester and give it one batch
+			TwitchIdAPIRequester requester = new TwitchIdAPIRequester(batch);
+			/// Add Future object to list of pending results
+			pendingResults.add(executor.submit(requester));
+			
+			numberOfApiCalls--;
+		}
+		
+		
+		
+		/// Loop through list of pending results to get the data
+		for (Future<SimpleTwitchUser[]> pendingResult : pendingResults) {
+			try {
+				/// Get one batch from the future object
+				/// Timeout if any single batch takes > 2 seconds
+				SimpleTwitchUser[] batch = 
+						(SimpleTwitchUser[]) pendingResult.get(2, TimeUnit.SECONDS);
+				
+				if (batch != null) {
+					for(SimpleTwitchUser usr : batch) {
+						result.add(usr);
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		executor.shutdownNow();
+		
+		return result;
+	}
+
 }
