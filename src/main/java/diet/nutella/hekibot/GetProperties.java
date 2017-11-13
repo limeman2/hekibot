1ddcc133a0cf694569d77e61b71a06c43c732db9
limeman2 <emilmurman@hotmail.com>
2017-11-13 20:55:45 +0100

    Enable reading redeem commands from a file

--- src/main/java/diet/nutella/hekibot/GetProperties.java
+++ src/main/java/diet/nutella/hekibot/GetProperties.java
@@ -1,19 +1,50 @@
 package main.java.diet.nutella.hekibot;
 
+import main.java.diet.nutella.hekibot.controller.RedeemCommand;
+
 import java.io.*;
-import java.util.Properties;
+import java.util.*;
 
 public class GetProperties {
-	private Properties props = new Properties();
-	
-	public GetProperties() {
+	public static Properties getConfigProperties() {
+		Properties result = new Properties();
 		InputStream inputStream = null;
+
 		try {
 			File jarPath = new File(GetProperties.class.getProtectionDomain().getCodeSource().getLocation().getPath());
 			String propertiesPath = jarPath.getParentFile().getAbsolutePath() + "/config.properties";
 
 			inputStream = new FileInputStream(propertiesPath);
- 
+
+			if (inputStream != null) {
+				result.load(inputStream);
+			} else {
+				throw new FileNotFoundException("property file '" + propertiesPath + "' not found in the classpath ");
+			}
+		} catch (Exception e) {
+			System.out.println("Exception: " + e);
+		} finally {
+			try {
+				inputStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return result;
+	}
+
+	public static Map<String, RedeemCommand> getRedeemCommands() {
+	    Map<String, RedeemCommand> res = new HashMap<>();
+		Properties props = new Properties();
+		InputStream inputStream = null;
+
+		try {
+		    File jarPath = new File(GetProperties.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+			String propertiesPath = jarPath.getParentFile().getAbsolutePath() + "/redeem_commands.properties";
+
+			inputStream = new FileInputStream(propertiesPath);
+
 			if (inputStream != null) {
 				props.load(inputStream);
 			} else {
@@ -28,10 +59,19 @@ public class GetProperties {
 				e.printStackTrace();
 			}
 		}
-	};
 
-	
-	public Properties getProperties() {
-		return this.props;
-	}
+		Scanner s;
+
+		for (Map.Entry<Object, Object> e : props.entrySet()) {
+		    s = new Scanner((String) e.getValue());
+		    s.useDelimiter(",");
+		    String cmd = s.next();
+		    int cost = s.nextInt();
+		    String response = s.next();
+
+		    res.put(cmd, new RedeemCommand(cmd, cost, response, null));
+        }
+
+        return res;
+    }
 }
